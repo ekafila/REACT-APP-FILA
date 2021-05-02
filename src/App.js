714870e0b7bda252381768877672f779bf074d90
ekafila <ekaterina.filiugina@gmail.com>
2021-05-02 23:58:54 +0300

    Разделение

--- src/App.js
+++ src/App.js
@@ -2,49 +2,11 @@
 import './App.css';
 import React from 'react';
 import TaskAdd from './TaskTracker/TaskTracker';
+import MyTodoList from './MyTodoList/MyTodoList.js';
 
-const TaskLog = (id, completed) => {
-  console.log('Task ' + id + ' completed status = ' + completed);
-}
-
-
-const Task = ({id, name, description, completed}) => {
-  
-  return (
-    <div>
-      <div>{id}</div>
-      <div>{name}</div>
-      <div>{description}</div>
-      <input type="checkbox" />
-      {/* <button className='complete-button' onClick={() => TaskLog(id, completed)}>Completed</button> */}
-    </div>
-     )
-    }
-
-
-class MyTodoList extends React.Component {
-  state = {
-    tasks: [{id: 1, name: 'Visit lecture', description: 'Read materials', completed: false},
-      {id: 2, name: 'Homework', description: 'Make React App', completed: true},
-      {id: 3, name: 'Dinner', description: 'Buy products', completed: false},
-      {id: 4, name: 'School', description: 'Make a presentation', completed: true},
-      {id: 5, name: 'Driving', description: 'Call the teacher', completed: true},
-      {id: 6, name: 'B-Day party', description: 'Find a present', completed: false}
-      ]
-    }
-
- 
-
-
-  render() {
-    return(
-      <div> 
-      {this.state.tasks.map(it => <Task id={it.id} name={it.name} description={it.description} completed={it.completed}/>)}
-      </div>
-      )
-    }
-  } 
-
+// const TaskLog = (id, completed) => {
+//   console.log('Task ' + id + ' completed status = ' + completed);
+// }
   
 
 function App () {
@@ -52,7 +14,8 @@ function App () {
     <div className='App'>
       <h1>Wonderful List</h1>
       <TaskAdd />
-      <button>Add</button>
+      <button onClick={new MyTodoList().handleClick}>Add</button>
+      {/* <button>Add</button> */}
       <main>
         <MyTodoList />
       </main>
@@ -61,36 +24,3 @@ function App () {
 }
 
 export default App;
-
-
- //   handleClick = (event) => {
-  //     this.setState(currentState => {
-  //         const newTasks = [...currentState.tasks]
-  //         const name = TaskAdd.state.name
-  //         const description = TaskAdd.state.description
-  //         const taskLength = newTasks.length
-  //         newTasks[taskLength] = {
-  //           id: taskLength + 1,
-  //           name: name,
-  //           description: description,
-  //           completed: false
-  //         }
-  //         return {
-  //             state: newTasks
-  //         }
-  //     })
-  //   }
-    
-  //   onChanges(id) {
-   
-  //     tasks = tasks.map(task => {
-  //       if (task.id === id) {
-  //         task.completed = !task.completed
-  //       }
-  //       return (
-  //         task
-  //       )
-  //     })
-  //   }
-    
-  // }
